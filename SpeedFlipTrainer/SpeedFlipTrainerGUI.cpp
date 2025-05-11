9d4d812bf718022ff70d923d74270bb3650f8623
Naai-san <m.martin.raynaud@gmail.com>
2025-05-11 13:10:26 +0200

    revert GUI

--- SpeedFlipTrainer/SpeedFlipTrainerGUI.cpp
+++ SpeedFlipTrainer/SpeedFlipTrainerGUI.cpp
@@ -1,270 +1,275 @@
 #include "pch.h"
 #include "SpeedFlipTrainer.h"
-// ImGuiFileDialog.h is already included via pch.h or SpeedFlipTrainer.h
-// BotAttempt.h is already included via SpeedFlipTrainer.h
+#include "ImGuiFileDialog.h"
+#include "BotAttempt.h"
 
 // Plugin Settings Window code here
 std::string SpeedFlipTrainer::GetPluginName() {
-	return "Speedflip Trainer"; // Corrected name
+	return "SpeedFlipTrainer";
 }
 
 // Render the plugin settings here
+// This will show up in bakkesmod when the plugin is loaded at
+//  f2 -> plugins -> SpeedFlipTrainer
 void SpeedFlipTrainer::RenderSettings() {
-	ImGui::TextUnformatted("Plugin to train speedflips in Musty's training pack: A503-264C-A7EB-D282");
+	ImGui::TextUnformatted("A plugin to help give training metrics when learning how to do a speedflip in Musty's training pack: A503-264C-A7EB-D282");
 
 	CVarWrapper enableCvar = cvarManager->getCvar("sf_enabled");
-	if (!enableCvar) { ImGui::TextUnformatted("Error: sf_enabled CVar not found!"); return; }
+	if (!enableCvar) return;
 
-	bool currentPluginEnabled = *enabled; // Use the bound variable
-	if (ImGui::Checkbox("Enable plugin", &currentPluginEnabled)) {
-		enableCvar.setValue(currentPluginEnabled);
-		*enabled = currentPluginEnabled; // Update bound variable
-	}
-	if (ImGui::IsItemHovered())
-		ImGui::SetTooltip("Enable/Disable Speedflip trainer plugin");
-
+	// DEBUT
 	ImGui::Separator();
 	ImGui::TextUnformatted("Car Axes Settings");
-
-	bool currentShowCarAxes = *showCarAxes;
-	if (ImGui::Checkbox("Show Car Axes", &currentShowCarAxes)) {
-		cvarManager->getCvar("sf_show_axes").setValue(currentShowCarAxes);
-		*showCarAxes = currentShowCarAxes;
-	}
+	ImGui::Checkbox("Show Car Axes", showCarAxes.get());
 	if (ImGui::IsItemHovered()) {
-		ImGui::SetTooltip("Display colored axes to help with orientation");
+		ImGui::SetTooltip("Display colored axes to help with orientation during aerial maneuvers");
 	}
 
 	if (*showCarAxes) {
-		// Use .get() to pass float* to ImGui::SliderFloat
-		if (ImGui::SliderFloat("Axes Length", axisLength.get(), 50.0f, 300.0f, "%.0f")) {
-			cvarManager->getCvar("sf_axis_length").setValue(*axisLength);
-		}
+		ImGui::SliderFloat("Axes Length", &axisLength, 50.0f, 300.0f, "%.0f");
 		if (ImGui::IsItemHovered()) {
 			ImGui::SetTooltip("Length of the orientation axes");
 		}
 	}
+	// FIN
 
+	bool enabled = enableCvar.getBoolValue();
+
+	if (ImGui::Checkbox("Enable plugin", &enabled))
+		enableCvar.setValue(enabled);
+	if (ImGui::IsItemHovered()) 
+		ImGui::SetTooltip("Enable/Disable Speeflip trainer plugin");
+
+	// ------------------------ ANGLE ----------------------------------
 	ImGui::Separator();
-	ImGui::TextUnformatted("Meter Display Settings");
+	{
+		CVarWrapper cvar = cvarManager->getCvar("sf_show_angle");
+		if (!cvar) return;
 
-	bool currentShowAngleMeter = *showAngleMeter;
-	if (ImGui::Checkbox("Show Dodge Angle Meter", &currentShowAngleMeter)) {
-		cvarManager->getCvar("sf_show_angle").setValue(currentShowAngleMeter);
-		*showAngleMeter = currentShowAngleMeter;
+		bool value = cvar.getBoolValue();
+
+		if (ImGui::Checkbox("Show dodge angle", &value))
+			cvar.setValue(value);
+		if (ImGui::IsItemHovered())
+			ImGui::SetTooltip("Show meter for the dodge angle.");
 	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("Show meter for the dodge angle.");
 
-	bool currentShowFlipMeter = *showFlipMeter;
-	if (ImGui::Checkbox("Show Flip Cancel Meter", &currentShowFlipMeter)) {
-		cvarManager->getCvar("sf_show_flip").setValue(currentShowFlipMeter);
-		*showFlipMeter = currentShowFlipMeter;
-	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("Show meter for the time to flip cancel.");
+	CVarWrapper leftAngleCvar = cvarManager->getCvar("sf_left_angle");
+	if (!leftAngleCvar) return;
 
-	bool currentShowJumpMeter = *showJumpMeter;
-	if (ImGui::Checkbox("Show First Jump Meter", &currentShowJumpMeter)) {
-		cvarManager->getCvar("sf_show_jump").setValue(currentShowJumpMeter);
-		*showJumpMeter = currentShowJumpMeter;
-	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("Show meter for time to first jump.");
+	int leftAngle = leftAngleCvar.getIntValue();
+	if (ImGui::SliderInt("Optimal left angle", &leftAngle, -70, -15))
+		leftAngleCvar.setValue(leftAngle);
+	if (ImGui::IsItemHovered())
+		ImGui::SetTooltip("The optimal angle at which to dodge left.");
 
-	bool currentShowPosMeter = *showPositionMeter;
-	if (ImGui::Checkbox("Show Horizontal Position Meter", &currentShowPosMeter)) {
-		cvarManager->getCvar("sf_show_position").setValue(currentShowPosMeter);
-		*showPositionMeter = currentShowPosMeter;
-	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("Show meter for the horizontal position.");
+	CVarWrapper rightAngleCvar = cvarManager->getCvar("sf_right_angle");
+	if (!rightAngleCvar) return;
 
-
+	int rightAngle = rightAngleCvar.getIntValue();
+	if (ImGui::SliderInt("Optimal right angle", &rightAngle, 15, 70))
+		rightAngleCvar.setValue(rightAngle);
+	if (ImGui::IsItemHovered())
+		ImGui::SetTooltip("The optimal angle at which to dodge right.");
+	// ------------------------ FLIP CANCEL ----------------------------------
 	ImGui::Separator();
-	ImGui::TextUnformatted("Gameplay Settings");
+	{
+		CVarWrapper cvar = cvarManager->getCvar("sf_show_flip");
+		if (!cvar) return;
 
-	if (ImGui::SliderInt("Optimal Left Angle", optimalLeftAngle.get(), -70, -15)) {
-		cvarManager->getCvar("sf_left_angle").setValue(*optimalLeftAngle);
+		bool value = cvar.getBoolValue();
+
+		if (ImGui::Checkbox("Show time to flip cancel", &value))
+			cvar.setValue(value);
+		if (ImGui::IsItemHovered())
+			ImGui::SetTooltip("Show meter for the time to flip cancel.");
 	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("The optimal angle (degrees) to dodge left.");
 
-	if (ImGui::SliderInt("Optimal Right Angle", optimalRightAngle.get(), 15, 70)) {
-		cvarManager->getCvar("sf_right_angle").setValue(*optimalRightAngle);
-	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("The optimal angle (degrees) to dodge right.");
+	CVarWrapper cancelCvar = cvarManager->getCvar("sf_cancel_threshold");
+	if (!cancelCvar) return;
 
-	if (ImGui::SliderInt("Flip Cancel Threshold (ticks)", flipCancelThreshold.get(), 1, 15)) {
-		cvarManager->getCvar("sf_cancel_threshold").setValue(*flipCancelThreshold);
-	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("Max physics ticks after dodge to perform flip cancel.");
-
-	// Commented out jumpLow/jumpHigh as they were in original GUI but cvars were commented in onLoad
-	// CVarWrapper jumpLowCVar = cvarManager->getCvar("sf_jump_low");
-	// CVarWrapper jumpHighCVar = cvarManager->getCvar("sf_jump_high");
-	// if (jumpLowCVar && jumpHighCVar) {
-	// 	int jumpPos[2] = {*jumpLow, *jumpHigh};
-	// 	if (ImGui::SliderInt2("First Jump Time (ticks)", jumpPos, 10, 120)) {
-	// 		if (jumpPos[0] < jumpPos[1]) {
-	// 			jumpLowCVar.setValue(jumpPos[0]); *jumpLow = jumpPos[0];
-	// 			jumpHighCVar.setValue(jumpPos[1]); *jumpHigh = jumpPos[1];
-	// 		} else {
-	// 			jumpLowCVar.setValue(jumpPos[1]); *jumpLow = jumpPos[1];
-	// 			jumpHighCVar.setValue(jumpPos[0]); *jumpHigh = jumpPos[0];
-	// 		}
-	// 	}
-	// 	if (ImGui::IsItemHovered()) ImGui::SetTooltip("Optimal tick range for the first jump.");
-	// }
+	int cancel = cancelCvar.getIntValue();
+	if (ImGui::SliderInt("Flip cancel threshold", &cancel, 1, 15))
+		cancelCvar.setValue(cancel);
+	if (ImGui::IsItemHovered())
+		ImGui::SetTooltip("The number of physics tick to perform flip cancel under.");
 
 
+	// ------------------------ FIRST JUMP ----------------------------------
 	ImGui::Separator();
-	ImGui::TextUnformatted("Game Speed Adjustment");
+	{
+		CVarWrapper cvar = cvarManager->getCvar("sf_show_jump");
+		if (!cvar) return;
 
-	bool currentChangeSpeed = *changeSpeed;
-	if (ImGui::Checkbox("Change Game Speed on Hit/Miss", &currentChangeSpeed)) {
-		cvarManager->getCvar("sf_change_speed").setValue(currentChangeSpeed);
-		*changeSpeed = currentChangeSpeed;
+		bool value = cvar.getBoolValue();
+
+		if (ImGui::Checkbox("Show time to first jump", &value))
+			cvar.setValue(value);
+		if (ImGui::IsItemHovered())
+			ImGui::SetTooltip("Show meter for time to first jump");
 	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("Alter game speed on consecutive hits/misses.");
 
-	bool currentRememberSpeed = *rememberSpeed;
-	if (ImGui::Checkbox("Remember Last Game Speed", &currentRememberSpeed)) {
-		cvarManager->getCvar("sf_remember_speed").setValue(currentRememberSpeed);
-		*rememberSpeed = currentRememberSpeed;
-	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("Restore last game speed on next training load.");
+	//CVarWrapper jumpLowCVar = cvarManager->getCvar("sf_jump_low");
+	//if (!jumpLowCVar) return;
+	//CVarWrapper jumpHighCVar = cvarManager->getCvar("sf_jump_high");
+	//if (!jumpHighCVar) return;
 
-	if (ImGui::SliderInt("Num Hits/Misses for Speed Change", numHitsChangedSpeed.get(), 1, 30)) {
-		cvarManager->getCvar("sf_num_hits").setValue(*numHitsChangedSpeed);
-	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("Consecutive hits/misses before speed changes.");
+	//int jumpPos[2];
+	//jumpPos[0] = jumpLowCVar.getIntValue();
+	//jumpPos[1] = jumpHighCVar.getIntValue();
+	//if (ImGui::SliderInt2("First jump time", jumpPos, 10, 120))
+	//{
+	//	if (jumpPos[0] < jumpPos[1])
+	//	{
+	//		jumpLowCVar.setValue(jumpPos[0]);
+	//		jumpHighCVar.setValue(jumpPos[1]);
+	//	}
+	//	else
+	//	{
+	//		jumpLowCVar.setValue(jumpPos[1]);
+	//		jumpHighCVar.setValue(jumpPos[0]);
+	//	}
+	//}
+	//if (ImGui::IsItemHovered())
+	//	ImGui::SetTooltip("The low and high thresholds for the first jump of the flip.");
 
-	if (ImGui::SliderFloat("Game Speed Increment", speedIncrement.get(), 0.001f, 0.5f, "%.3f")) { // Max to 0.5, original was 0.999
-		cvarManager->getCvar("sf_speed_increment").setValue(*speedIncrement);
-	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("Amount to change game speed by.");
-
-
+	// ------------------------ POSITION ----------------------------------
 	ImGui::Separator();
-	ImGui::TextUnformatted("Attempt Management");
-	bool currentSaveToFile = *saveToFile;
-	if (ImGui::Checkbox("Save Attempts to File", &currentSaveToFile)) {
-		cvarManager->getCvar("sf_save_attempts").setValue(currentSaveToFile);
-		*saveToFile = currentSaveToFile;
-	}
-	if (ImGui::IsItemHovered()) ImGui::SetTooltip("Automatically save each attempt's inputs to a file.");
+	{
+		CVarWrapper cvar = cvarManager->getCvar("sf_show_position");
+		if (!cvar) return;
 
+		bool value = cvar.getBoolValue();
+
+		if (ImGui::Checkbox("Show horizontal position", &value))
+			cvar.setValue(value);
+		if (ImGui::IsItemHovered())
+			ImGui::SetTooltip("Show meter for the horizontal position.");
+	}
+
+	// ------------------------ SPEED SETTINGS ----------------------------------
+	ImGui::Separator();
+	CVarWrapper changeSpeedCvar = cvarManager->getCvar("sf_change_speed");
+	if (!changeSpeedCvar) return;
+
+	bool changeSpeed = changeSpeedCvar.getBoolValue(); 
+
+	if (ImGui::Checkbox("Change game speed on hit/miss", &changeSpeed))
+		changeSpeedCvar.setValue(changeSpeed);
+	if (ImGui::IsItemHovered())
+		ImGui::SetTooltip("If checked this will alter the game speed on consecutive hits or misses.");
+
+	CVarWrapper remSpeedCvar = cvarManager->getCvar("sf_remember_speed");
+	if (!remSpeedCvar) return;
+
+	bool remSpeed = remSpeedCvar.getBoolValue();
+
+	if (ImGui::Checkbox("Remember last game speed:", &remSpeed))
+		remSpeedCvar.setValue(remSpeed);
+	if (ImGui::IsItemHovered())
+		ImGui::SetTooltip("Remember last game speed achieved on next load of training session.");
+
+	CVarWrapper hitsCvar = cvarManager->getCvar("sf_num_hits");
+	if (!hitsCvar) return;
+
+	int hit = hitsCvar.getIntValue();
+	if (ImGui::SliderInt("Number hits/misses before speed change", &hit, 1, 30))
+		hitsCvar.setValue(hit);
+	if (ImGui::IsItemHovered())
+		ImGui::SetTooltip("Number of consecutive hits/misses before speed change.");
+
+	CVarWrapper speedIncCvar = cvarManager->getCvar("sf_speed_increment");
+	if (!speedIncCvar) return;
+
+	float speedInc = speedIncCvar.getFloatValue();
+	if (ImGui::SliderFloat("Value of speed increment", &speedInc, 0.001, 0.999))
+		speedIncCvar.setValue(speedInc);
+	if (ImGui::IsItemHovered())
+		ImGui::SetTooltip("The value to add or subtract from game speed.");
 }
 
 
-// Render the plugin GUI window
+// Do ImGui rendering here
 void SpeedFlipTrainer::Render()
 {
 	if (!ImGui::Begin(menuTitle_.c_str(), &isWindowOpen_, ImGuiWindowFlags_None))
 	{
+		// Early out if the window is collapsed, as an optimization.
 		ImGui::End();
 		return;
 	}
 
-	if (ImGui::Button("Enable Manual Mode"))
+	if (ImGui::Button("Enable manual mode"))
 	{
 		mode = SpeedFlipTrainerMode::Manual;
 		LOG("MODE = Manual");
 	}
 	ImGui::SameLine();
-	if (ImGui::Button("Save Last Attempt"))
+	if (ImGui::Button("Save last attempt"))
 	{
-		if (!attempt.inputs.empty()) {
-			auto path = attempt.GetFilename(dataDir); // Ensure dataDir is initialized
-			attempt.WriteInputsToFile(path);
-			LOG("Saved attempt to: {}", path.string());
-		}
-		else {
-			LOG("No inputs in last attempt to save.");
-		}
+		auto path = attempt.GetFilename(dataDir);
+		attempt.WriteInputsToFile(path);
+		LOG("Saved attempt to: {0}", path.string());
 	}
 
-	if (ImGui::Button("Replay Last Attempt"))
+	if (ImGui::Button("Replay last attempt"))
 	{
-		if (!attempt.inputs.empty()) {
-			mode = SpeedFlipTrainerMode::Replay;
-			LOG("MODE = Replay (Last Attempt)");
-			replayAttempt = attempt;
-		}
-		else {
-			LOG("No inputs in last attempt to replay.");
-		}
+		mode = SpeedFlipTrainerMode::Replay;
+		LOG("MODE = Replay");
+		replayAttempt = attempt;
 	}
 	ImGui::SameLine();
-	if (ImGui::Button("Load Replay Attempt from File"))
+	if (ImGui::Button("Load replay attempt"))
 	{
-		attemptFileDialog.Open(); // Use Open() instead of .open = true
+		attemptFileDialog.open = true;
 	}
-	// Use Display() and IsOpened() for ImGuiFileDialog v0.6+
-	if (attemptFileDialog.IsOpened()) {
-		attemptFileDialog.Display();
-		if (attemptFileDialog.HasSelected()) {
-			try {
-				Attempt loadedAttempt; // Create new attempt object
-				loadedAttempt.ReadInputsFromFile(attemptFileDialog.GetSelected().string()); // Use GetSelected().string()
-				if (!loadedAttempt.inputs.empty()) {
-					mode = SpeedFlipTrainerMode::Replay;
-					LOG("MODE = Replay (File)");
-					replayAttempt = loadedAttempt; // Assign to replayAttempt
-					LOG("Loaded attempt from file: {}", attemptFileDialog.GetSelected().string());
-				}
-				else {
-					LOG("Attempt file was empty or failed to load: {}", attemptFileDialog.GetSelected().string());
-				}
-			}
-			catch (const std::exception& e) {
-				LOG("Failed to read attempt from file: {}. Exception: {}", attemptFileDialog.GetSelected().string(), e.what());
-			}
-			catch (...) {
-				LOG("Failed to read attempt from file: {} (Unknown exception)", attemptFileDialog.GetSelected().string());
-			}
-			attemptFileDialog.ClearSelected();
-			attemptFileDialog.Close();
+	if (attemptFileDialog.open && attemptFileDialog.ShowFileDialog(ImGui::FileDialogType::SelectFile))
+	{
+		try 
+		{
+			auto a = Attempt();
+			a.ReadInputsFromFile(attemptFileDialog.selected);
+			mode = SpeedFlipTrainerMode::Replay;
+			LOG("MODE = Replay");
+			replayAttempt = a;
+			LOG("Loaded attempt from file: {0}", attemptFileDialog.selected.string());
+		}
+		catch (...)
+		{
+			LOG("Failed to read attempt from file: {0}", attemptFileDialog.selected.string());
 		}
 	}
 
-
-	if (ImGui::Button("Load Bot: Diagonal Flip (-26 deg)")) // More descriptive
+	if (ImGui::Button("Load -26 Bot"))
 	{
 		bot.Become26Bot();
 		mode = SpeedFlipTrainerMode::Bot;
-		LOG("MODE = Bot (-26 Degree)");
+		LOG("MODE = Bot");
 	}
 	ImGui::SameLine();
-	if (ImGui::Button("Load Bot: Forward Diagonal Flip (-45 deg)")) // More descriptive
+	if (ImGui::Button("Load -45 Bot"))
 	{
 		bot.Become45Bot();
 		mode = SpeedFlipTrainerMode::Bot;
-		LOG("MODE = Bot (-45 Degree)");
+		LOG("MODE = Bot");
 	}
 	ImGui::SameLine();
-	if (ImGui::Button("Load Bot from File"))
+	if (ImGui::Button("Load Bot"))
 	{
-		botFileDialog.Open();
+		botFileDialog.open = true;
 	}
-	if (botFileDialog.IsOpened()) {
-		botFileDialog.Display();
-		if (botFileDialog.HasSelected()) {
-			try {
-				bot.ReadInputsFromFile(botFileDialog.GetSelected().string()); // Directly use bot member
-				if (!bot.inputs.empty()) {
-					LOG("Loaded bot from file: {}", botFileDialog.GetSelected().string());
-					mode = SpeedFlipTrainerMode::Bot;
-					LOG("MODE = Bot (File)");
-				}
-				else {
-					LOG("Bot file was empty or failed to load: {}", botFileDialog.GetSelected().string());
-				}
-			}
-			catch (const std::exception& e) {
-				LOG("Failed to read bot from file: {}. Exception: {}", botFileDialog.GetSelected().string(), e.what());
-			}
-			catch (...) {
-				LOG("Failed to read bot from file: {} (Unknown exception)", botFileDialog.GetSelected().string());
-			}
-			botFileDialog.ClearSelected();
-			botFileDialog.Close();
+	if (botFileDialog.open && botFileDialog.ShowFileDialog(ImGui::FileDialogType::SelectFile))
+	{
+		try
+		{
+			bot.ReadInputsFromFile(botFileDialog.selected);
+			LOG("Loaded bot from file: {0}", botFileDialog.selected.string());
+			mode = SpeedFlipTrainerMode::Bot;
+			LOG("MODE = Bot");
+		}
+		catch (...)
+		{
+			LOG("Failed to read bot from file: {0}", botFileDialog.selected.string());
 		}
 	}
 
@@ -276,38 +281,43 @@ void SpeedFlipTrainer::Render()
 	}
 }
 
+// Name of the menu that is used to toggle the window.
 std::string SpeedFlipTrainer::GetMenuName()
 {
-	return "speedfliptrainer"; // Conventionally lowercase and one word
+	return "SpeedFlipTrainer";
 }
 
+// Title to give the menu
 std::string SpeedFlipTrainer::GetMenuTitle()
 {
 	return menuTitle_;
 }
 
+// Don't call this yourself, BM will call this function with a pointer to the current ImGui context
 void SpeedFlipTrainer::SetImGuiContext(uintptr_t ctx)
 {
 	ImGui::SetCurrentContext(reinterpret_cast<ImGuiContext*>(ctx));
-	// Optional: Setup ImGuiFileDialog if it needs the context explicitly
-	// ImGui::FileDialog::Instance().SetCurrentContext(reinterpret_cast<ImGuiContext*>(ctx));
 }
 
+// Should events such as mouse clicks/key inputs be blocked so they won't reach the game
 bool SpeedFlipTrainer::ShouldBlockInput()
 {
 	return ImGui::GetIO().WantCaptureMouse || ImGui::GetIO().WantCaptureKeyboard;
 }
 
+// Return true if window should be interactive
 bool SpeedFlipTrainer::IsActiveOverlay()
 {
-	return true; // Window is interactive
+	return true;
 }
 
+// Called when window is opened
 void SpeedFlipTrainer::OnOpen()
 {
 	isWindowOpen_ = true;
 }
 
+// Called when window is closed
 void SpeedFlipTrainer::OnClose()
 {
 	isWindowOpen_ = false;
